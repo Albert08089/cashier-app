3663e1d559d813851ba1e1ecad85366f2312aa3d
Albert08089 <34734515+Albert08089@users.noreply.github.com>
2021-07-29 22:58:38 -0500

    CLion ide update

---
+++ cmake-build-debug/CMakeFiles/cashier_app.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/alber/OneDrive/Desktop/code/cashier-app/storecashierprogram/Brand.cpp" "C:/Users/alber/OneDrive/Desktop/code/cashier-app/cmake-build-debug/CMakeFiles/cashier_app.dir/storecashierprogram/Brand.cpp.obj"
+  "C:/Users/alber/OneDrive/Desktop/code/cashier-app/storecashierprogram/cashregister.cpp" "C:/Users/alber/OneDrive/Desktop/code/cashier-app/cmake-build-debug/CMakeFiles/cashier_app.dir/storecashierprogram/cashregister.cpp.obj"
+  "C:/Users/alber/OneDrive/Desktop/code/cashier-app/storecashierprogram/coupon.cpp" "C:/Users/alber/OneDrive/Desktop/code/cashier-app/cmake-build-debug/CMakeFiles/cashier_app.dir/storecashierprogram/coupon.cpp.obj"
+  "C:/Users/alber/OneDrive/Desktop/code/cashier-app/storecashierprogram/main.cpp" "C:/Users/alber/OneDrive/Desktop/code/cashier-app/cmake-build-debug/CMakeFiles/cashier_app.dir/storecashierprogram/main.cpp.obj"
+  "C:/Users/alber/OneDrive/Desktop/code/cashier-app/storecashierprogram/manager.cpp" "C:/Users/alber/OneDrive/Desktop/code/cashier-app/cmake-build-debug/CMakeFiles/cashier_app.dir/storecashierprogram/manager.cpp.obj"
+  "C:/Users/alber/OneDrive/Desktop/code/cashier-app/storecashierprogram/storeitem.cpp" "C:/Users/alber/OneDrive/Desktop/code/cashier-app/cmake-build-debug/CMakeFiles/cashier_app.dir/storecashierprogram/storeitem.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../storecashierprogram"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cashier_app.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cashier_app.dir/storecashierprogram/Brand.cpp.obj"
+  "CMakeFiles/cashier_app.dir/storecashierprogram/cashregister.cpp.obj"
+  "CMakeFiles/cashier_app.dir/storecashierprogram/coupon.cpp.obj"
+  "CMakeFiles/cashier_app.dir/storecashierprogram/main.cpp.obj"
+  "CMakeFiles/cashier_app.dir/storecashierprogram/manager.cpp.obj"
+  "CMakeFiles/cashier_app.dir/storecashierprogram/storeitem.cpp.obj"
+  "cashier_app.exe"
+  "cashier_app.exe.manifest"
+  "cashier_app.lib"
+  "cashier_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cashier_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
